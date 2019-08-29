6d0eac83edf105362d4c9395b833212d6490b3be
Dinda Ayu P <dindaayuprtw@gmail.com>
2019-08-29 18:32:12 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,23 +6,53 @@ const bot = new TelegramBot(token, {
 const gojec = require('./gojec.js')
 
 const StartKeyboard = [
-    ['a', 'b'],
-    ['Contact Us']
-  ]
+    ['/gojec'],
+    ['/info']
+]
 
 // YOUR CODE STARTS HERE
 
 bot.onText(/\/start/, (msg) => {
     const opts = {
-      reply_to_message_id: msg.message_id,
-      reply_markup: JSON.stringify({
-        keyboard: StartKeyboard,
-        resize_keyboard: true,
-        one_time_keyboard: true
-      })
+        reply_to_message_id: msg.message_id,
+        reply_markup: JSON.stringify({
+            keyboard: StartKeyboard,
+            resize_keyboard: true,
+            one_time_keyboard: true
+        })
     };
-    bot.sendMessage(msg.chat.id, `Hello`, opts);
-  });
+    bot.sendMessage(msg.chat.id, `Hii`, opts);
+});
+
+
+bot.onText(/\/gojec/, (msg) => {
+    const opts = {
+        reply_to_message_id: msg.message_id
+    };
+    bot.sendMessage(msg.chat.id, `Number? (62849#####):`, opts);
+    resmessage();
+});
+
+// Functions
+var getmessage = async () => {
+    await new Promise((resolve, reject) => {
+        bot.once('message', (msg) => {
+            console.log("User Message Is: " + msg.text)
+            const opts = {
+                reply_to_message_id: msg.message_id,
+            };
+            bot.sendMessage(msg.chat.id, 'Thanks, Your Request Received', opts);
+            resolve(true);
+        });
+    });
+    return
+}
+
+var resmessage = async () => {
+    await getmessage();
+}
+
+
 
 
 
@@ -40,7 +70,7 @@ bot.on('message', (msg) => {
     if (text == '/gojec') {
         var nomer = text.split(' ').splice(1).join(' ');
         //try {
-       //     var send = await gojec.doStuff(nomer)
+        //     var send = await gojec.doStuff(nomer)
         //} catch (e) {}
         //bot.sendMessage(msg.chat.id, send);
     }
