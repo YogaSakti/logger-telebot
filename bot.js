65b17b411e678840a1a48d02f61a319b2af6ef8d
Dinda Ayu P <dindaayuprtw@gmail.com>
2019-08-29 17:58:42 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,29 +7,27 @@ const gojec = require('./gojec.js')
 
 
 // YOUR CODE STARTS HERE
-
-;(async () => {
-    try {
-        bot.on('message', (msg) => {
-            const text = msg.text
-            if (text == '/info') {
-                var info = 'Chat id: ' + msg.chat.id + ' Sender: ' + msg.from.username;
-                bot.sendMessage(msg.chat.id, info);
-            }
-            if (text == '/login-ig') {
-                var info = 'Chat id: ' + msg.chat.id + ' Sender: ' + msg.from.username;
-                bot.sendMessage(msg.chat.id, info);
-            }
-        
-            if (text == '/gojec') {
-                var nomer = text.split(' ').splice(1).join(' ');
-                const send = await gojec.doStuff(nomer)
-                //console.log(send)
-                bot.sendMessage(msg.chat.id, send);
-            }
-        
-        });
-    } catch (e) {
-        
+bot.on('message', (msg) => {
+    const text = msg.text
+    if (text == '/info') {
+        var info = 'Chat id: ' + msg.chat.id + ' Sender: ' + msg.from.username;
+        bot.sendMessage(msg.chat.id, info);
     }
-})();
+    if (text == '/login-ig') {
+        var info = 'Chat id: ' + msg.chat.id + ' Sender: ' + msg.from.username;
+        bot.sendMessage(msg.chat.id, info);
+    }
+
+    if (text == '/gojec') {
+        var nomer = text.split(' ').splice(1).join(' ');
+        ;(async () => {
+            try {
+                const send = await gojec.doStuff(nomer)
+                return send
+            } catch (e) { 
+            }
+        })();
+        bot.sendMessage(msg.chat.id, send);
+    }
+
+});
