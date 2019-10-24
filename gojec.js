c95a7ca223a879fb5b9d4513343dae6c383f493b
Yogs <yogazacty@gmail.com>
2019-10-24 23:24:13 +0700

    kebalik anjay

--- gojec.js
+++ gojec.js
@@ -94,8 +94,8 @@ const trnsfr = (accessToken, uuid, uniqid, qrid) => new Promise((resolve, reject
 async function doStuff(Number) {
 	const uniqueid = await genUniqueId(16);
 	const qrid = await getqr(accessToken, uuid, uniqueid, Number)
-	console.log(qrid)
-	if (!qrid.success) {
+	// console.log(qrid)
+	if (qrid.success) {
 		const kirimsaldo = await trnsfr(accessToken, uuid, uniqueid, qrid.data.qr_id)
 		console.log(kirimsaldo)
 		return kirimsaldo
